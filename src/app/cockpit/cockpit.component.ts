cd8b0e8e286dd0e9318332629e1c05846cec8fb8
anand <anand10parmar@gmail.com>
2018-05-30 09:38:37 -0400

    first

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,40 @@
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {EventListener} from "@angular/core/src/debug/debug_node";
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  @Output('svCreated') serverCreated = new EventEmitter<{serverName: string,serverContent: string}>();
+
+
+  @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput : ElementRef;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer(nameInput: HTMLInputElement) {
+
+  this.serverCreated.emit({
+    serverName : nameInput.value,
+    serverContent: this.serverContentInput.nativeElement.value});
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.bluePrintCreated.emit({
+      serverName : nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value});
+  }
+
+
+}
